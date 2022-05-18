370361720bbb0c38c5b47108b18b0623faae1738
sousouben <s.mouatakide@laposte.net>
2022-05-18 11:23:49 +0200

    installation de package.json eslint et gitignore

--- scripts/pages/photographer.js
+++ scripts/pages/photographer.js
@@ -1,1 +1,13 @@
 //Mettre le code JavaScript lié à la page photographer.html
+
+//Renvoi vers la page du photographe ciblé
+const url = new URL(window.location.href);
+const photographerId = url.searchParams.get("id");
+
+async function getMedias() {
+    //Récupération ressources JSON
+    const response = await fetch('data/photographers.json')
+    const data = await response.json()
+    return data;
+}
+
