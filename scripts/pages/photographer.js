1a8f8514f0a9c05a5dc377c185faba14f621db6c
sousouben <s.mouatakide@laposte.net>
2022-06-04 18:13:21 +0200

    media queries pour iPhone 6

--- scripts/pages/photographer.js
+++ scripts/pages/photographer.js
@@ -117,11 +117,11 @@ function ajoutLikes() {
     //EVENEMENT AU CLICK
     e.addEventListener("click", function () {
       // au click sur l'element
-
       const nbreLike = e.parentElement.children[1]; //creation constante qui cible le nbre de like
       nbreLike.textContent++; // j'aoute 1 au nbre de like
       let totalLikes = document.getElementById("total_likes"); // je cible le total des likes dans le bandeau
       totalLikes.innerHTML++; // j'ajoute 1 a ce total
+      
     });
 
     //EVENEMENT AU CLAVIER AVEC TOUCHE ENTREE
